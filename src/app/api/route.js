e36f81a176b0b590936276609976fd3f20bf8360
shihab-newaz <shihab.newaz.5633@gmail.com>
2024-09-17 21:18:49 +0600

    trying deployment

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,4 +1,4 @@
-// import { StreamChat } from 'stream-chat';
+import { StreamChat } from 'stream-chat';
 import twilio from 'twilio';
 import { NextResponse } from 'next/server';
 
@@ -7,12 +7,22 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const messageSID = process.env.TWILIO_MSID;
 const twilioClient = twilio(accountSID, authToken);
 
+// Stream Chat client initialization
+const api_key = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const serverClient = StreamChat.getInstance(api_key, api_secret);
+
 export async function GET() {
   return NextResponse.json({ message: "Server is running" });
 }
 
 export async function POST(request) {
   const { message, user: sender, type, members } = await request.json();
+  
+  if (!sender || !sender.id) {
+    return NextResponse.json({ error: "Username is required" }, { status: 400 });
+  }
+
   if (type === "message.new") {
     members
       .filter((member) => member.user_id !== sender.id)
